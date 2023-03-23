058be9ee2e0307922a63fb4ae0581798ee2892a4
Jayce(최현범) <hb.choi@dreamus.io>
2023-03-23 16:12:46 +0900

    Initial commit

---
+++ src/test/java/com/example/chatgptcodereviewtest/ChatGptCodeReviewTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptcodereviewtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptCodeReviewTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
